83bac74adb95ac6b8769c0542a34a4ccf65b0c57
larry <changyu.liu@alphabetter.com.cn>
2024-02-26 11:13:48 +0800

    upgrade

--- real_esrgan/models/edsrnet.py
+++ real_esrgan/models/edsrnet.py
@@ -33,17 +33,18 @@ class EDSRNet(nn.Module):
             channels: int = 64,
             num_rcb: int = 16,
             upscale_factor: int = 4,
+            image_range: float = 255.,
             mean: Tensor = None,
     ) -> None:
         super(EDSRNet, self).__init__()
         assert upscale_factor in (2, 3, 4, 8), "Upscale factor should be 2, 3, 4 or 8."
 
-        if mean is not None:
-            self.register_buffer("mean", mean.view(1, 3, 1, 1))
-        else:
-            # DIV2K mean
-            self.register_buffer("mean", Tensor([0.4488, 0.4371, 0.4040]).view(1, 3, 1, 1))
         self.upscale_factor = upscale_factor
+        if mean is not None:
+            self.mean = torch.tensor(mean).view(1, 3, 1, 1)
+        else:
+            self.mean = torch.tensor([0.4488, 0.4371, 0.4040]).view(1, 3, 1, 1)
+        self.image_range = image_range
 
         # First layer
         self.conv_1 = nn.Conv2d(in_channels, channels, 3, stride=1, padding=1)
@@ -72,15 +73,15 @@ class EDSRNet(nn.Module):
         initialize_weights(self.modules())
 
     def forward(self, x: Tensor) -> Tensor:
-        out = x.sub_(self.mean).mul_(255.)
+        out = x.sub_(self.mean).mul_(self.image_range)
 
-        out1 = self.conv_1(out)
-        out = self.trunk(out1)
+        conv_1 = self.conv_1(out)
+        out = self.trunk(conv_1)
         out = self.conv_2(out)
-        out = torch.add(out, out1)
+        out = torch.add(out, conv_1)
         out = self.up_sampling(out)
         out = self.conv_3(out)
-        out = out.div_(255.).add_(self.mean)
+        out = out.div_(self.image_range).add_(self.mean)
         return torch.clamp_(out, 0.0, 1.0)
 
 
